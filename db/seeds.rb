90be2282b551bd5296e694753ee742d032f74181
marie normand <normandmarie1998@gmail.com>
2022-11-15 16:38:56 +0100

    exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,12 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   Character.create(name: "Luke", movie: movies.first)require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Coffee.origin,
+    content: Faker::Lorem
+  )
+  article.save
+end
